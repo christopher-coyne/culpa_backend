84cc01c440331f74ce3cdfe5fc82625f37efa06a
christopher coyne <christcoyne@gmail.com>
2022-02-19 01:09:07 -0500

    added backend query for single prof

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const format = require('pg-format');
 const { Pool, Client } = require('pg')
+const fs = require('fs')
 
 const app = express()
 
@@ -14,21 +15,40 @@ const client = new Client({
   })
   client.connect();
 
-app.get('/search/:term', (req, res) => {
-  const sql = "SELECT * FROM professors"
+app.get('/search-prof-name/:term', (req, res) => {
+  const sql = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term)
     client.query(sql).then((result, err) => {
       res.json({'profs': result.rows})
     })
-    // res.send(`you searched for : ${req.params.term}`)
 })
 
-app.get('/test/:term', (req, res) => {
-  const sql = format("SELECT courses.name AS name, courses.course_id AS id, '1' as type FROM courses WHERE courses.name LIKE '%%%s%%' UNION ALL SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term, req.params.term)
+app.get('/get-prof-reviews/:term', (req, res) => {
+  // const sql_reviews = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term)
+  // const prof_data = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term)
+
+  // for now, try to find all data on all tables
+  const sql = format("SELECT * from reviews WHERE reviews.prof_id = '%s';", req.params.term)
+  const sql2 = format("SELECT * from professors WHERE professors.prof_id = '%s';", req.params.term)
+
+  const return_value = {"professor": null, "reviews": []}
+
+    client.query(sql).then((result, err) => {
+      return_value.reviews = result.rows
+      client.query(sql2).then((result, err) => {
+        return_value.professor = result.rows
+        res.json({'results': return_value})
+      })
+    })
+
+})
+
+app.get('/match-term-all/:term', (req, res) => {
+  const sql = format("SELECT courses.name AS name, courses.course_id AS id, 'course' as type FROM courses WHERE courses.name LIKE '%%%s%%' UNION ALL SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term, req.params.term)
   console.log('sql format : ', sql)
   client.query(sql).then((result, err) => {
       console.log('res ', result)
       console.log('err ', err)
-      res.json({'courseProfs': result.rows})
+      res.json({'results': result.rows})
       // console.log('erros ? ', x)
     }).catch((e) => {
       console.log('error', e)
