2fa95f63d56f57cd62066948188a7865ef34ba64
christopher coyne <christcoyne@gmail.com>
2022-03-26 23:04:11 -0400

    added submit post route to server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const format = require('pg-format');
+const bodyParser = require('body-parser')
 const { Pool, Client } = require('pg')
 const fs = require('fs')
 
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 /* postgresql stuff */
 const client = new Client({
@@ -16,21 +20,44 @@ const client = new Client({
   client.connect();
 
 app.get('/search-prof-name/:term', (req, res) => {
-  const sql = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term)
+  const sql = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%%';", req.params.term)
+  console.log('sql : ', sql)
     client.query(sql).then((result, err) => {
+      console.log("results : ", result)
+      res.json({'profs': result.rows})
+  })
+})
+
+app.get('/search-course-name/:term', (req, res) => {
+  const sql = format("SELECT courses.name, courses.course_id FROM courses WHERE courses.name LIKE '%%%s%%%';", req.params.term)
+  console.log('sql : ', sql)
+    client.query(sql).then((result, err) => {
+      console.log("results : ", result)
+      res.json({'courses': result.rows})
+  })
+})
+
+
+app.get('/get-all-profs', (req, res) => {
+  const sql = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors;")
+    client.query(sql).then((result, err) => {
+      console.log("results : ", result)
       res.json({'profs': result.rows})
     })
 })
 
-app.get('/get-prof-reviews/:term', (req, res) => {
+app.get('/get-professor-reviews/:term', (req, res) => {
   // const sql_reviews = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term)
   // const prof_data = format("SELECT professors.name, professors.prof_id, professors.nugget FROM professors WHERE professors.name LIKE '%%%s%%';", req.params.term)
 
+  // want to return professor info, the reviews, and the names of the courses
+
   // for now, try to find all data on all tables
-  const sql = format("SELECT * from reviews WHERE reviews.prof_id = '%s';", req.params.term)
+  // const sql = format("SELECT * from reviews WHERE reviews.prof_id = '%s';", req.params.term)
+  const sql = format("SELECT reviews.agree, reviews.disagree, reviews.content, reviews.workload, reviews.date, courses.name, courses.course_id from reviews INNER JOIN courses ON courses.course_id = reviews.course_id WHERE reviews.prof_id = '%s';", req.params.term)
   const sql2 = format("SELECT * from professors WHERE professors.prof_id = '%s';", req.params.term)
 
-  const return_value = {"professor": null, "reviews": []}
+  const return_value = {"professor": {}, "reviews": []}
 
     client.query(sql).then((result, err) => {
       return_value.reviews = result.rows
@@ -53,6 +80,12 @@ app.get('/match-term-all/:term', (req, res) => {
     }).catch((e) => {
       console.log('error', e)
     })
+})
+
+app.post('/submit', (req, res) => {
+  console.log('body of post : ', req.body)
+
+  res.json({'professor': false, 'course': false, 'workload': false, 'content': false})
 })
 
 app.listen(5000, console.log('server running on port 5000'))
